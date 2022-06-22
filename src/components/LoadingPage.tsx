5343fc2a0ef1de1234b23d0e3b803ec2e9bde338
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-06-22 17:39:35 +0700

    create loading page

---
+++ src/components/LoadingPage.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./LoadingPage.scss";
+
+const LoadingPage: React.FC = () => {
+  return (
+    <div className="loading-page">
+      <div className="loadingio-spinner-spin-x6fzcj3pud">
+        <div className="ldio-wa01m88rjr">
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
